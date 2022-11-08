fd3e01f030c7f0b8fe4a35025c4e6b236707988d
ziyi <daydreamnation@live.com>
2022-11-08 21:58:57 +0100

    feat(): add image slider

---
+++ kasa/src/components/ImageSlider.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+type Props = {
+  slides: string[];
+};
+const ImageSlider = ({ slides }: Props) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const sliderStyles = {
+    height: "100%",
+    position: "relative",
+  } as React.CSSProperties;
+  const slideStyles = {
+    width: "100%",
+    height: "100%",
+    borderRadius: 5,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundImage: `url(${slides[currentIndex]})`,
+  } as React.CSSProperties;
+
+  const leftArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    left: 32,
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  } as React.CSSProperties;
+
+  const rightArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translate(0, -50%)",
+    right: 32,
+    fontSize: "45px",
+    color: "#fff",
+    zIndex: 1,
+    cursor: "pointer",
+  } as React.CSSProperties;
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  return (
+    <div style={sliderStyles}>
+      {slides.length && (
+        <>
+          <div style={leftArrowStyles} onClick={goToPrevious}>
+            {"<"}
+          </div>
+          <div style={rightArrowStyles} onClick={goToNext}>
+            {">"}
+          </div>
+        </>
+      )}
+      <div style={slideStyles}></div>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- kasa/src/pages/Logement.tsx
+++ kasa/src/pages/Logement.tsx
@@ -1,8 +1,38 @@
 import { useLoaderData } from "react-router-dom";
+import ImageSlider from "../components/ImageSlider";
+
+type Host = {
+  name: string;
+  picture: string;
+};
+type Data = {
+  id: string;
+  title: string;
+  cover: string;
+  pictures: string[];
+  description: string;
+  host: Host;
+  rating: string;
+  location: string;
+  Amenities: string[];
+  tags: string[];
+};
 
 export const Logement = () => {
-  const data: any = useLoaderData();
+  const data = useLoaderData() as Data;
 
   console.log("data is", data);
-  return <div>{data?.title}</div>;
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: 415,
+          margin: "0 auto",
+        }}
+      >
+        <ImageSlider slides={data.pictures} />
+      </div>
+    </>
+  );
 };
